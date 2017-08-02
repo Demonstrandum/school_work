d453c90b32bd10e176b1c027abe50bc931b676d7
Demonstrandum <knutsen@jetspace.co>
2017-08-02 04:47:14 +0100

    Fancy underlining

--- summer_transition_pack/products/productsList/__init__.py
+++ summer_transition_pack/products/productsList/__init__.py
@@ -11,15 +11,16 @@ def main():
     summation   = sum(product[1] for product in unarranged)
     discountSum = sum(discount[1] for discount in discounted)
     average     = summation / len(arranged) # or unarranged, doesn't matter.
+    withVAT     = discountSum + discountSum * 0.2
 
     print('\n\n\'{}\' is the most expensive at: £{:.2f}'.format(most[0], most[1]))
     print('\'{}\' is the cheapes at: £{:.2f}\n'.format(least[0], least[1]))
 
     print('Average cost among all items is: £{:.2f}\n'.format(average))
 
-    print('Summation of product costs is:          £{:.2f}'.format(summation))
+    print('Summation of products\' costs is:        £{:.2f}'.format(summation))
     print('With 5% discount on products over £50:  £{:.2f}\n'.format(discountSum))
-    print('Final cost with VAT of 20%:             £{:.2f}\n'.format(discountSum + (discountSum * 0.2)))
-
+    print('Final cost with VAT of 20%:             £{:.2f}'.format(withVAT))
+    print('===========================             ' + '=' * len('£{:.2f}'.format(withVAT)))
 if __file__ == '__main__':
     main()
